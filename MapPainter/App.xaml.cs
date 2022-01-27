30066f1791d6f91eacc867d9163ad452f6b502c0
neutro <rodionkush903@gmail.com>
2022-01-27 20:16:01 +0300

    points

--- MapPainter/App.xaml.cs
+++ MapPainter/App.xaml.cs
@@ -8,10 +8,8 @@ using System.Windows;
 
 namespace MapPainter
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- MapPainter/MainWindow.xaml.cs
+++ MapPainter/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,14 +16,28 @@ using System.Windows.Shapes;
 
 namespace MapPainter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<StylusPoint> points = new();
+            foreach (Stroke s in ink.Strokes)
+                foreach (StylusPoint sp in s.StylusPoints)
+                {
+                    points.Add(sp);
+                    coordinates.Text = $"x:{sp.X}, y:{sp.Y}";
+                }
+            MessageBox.Show($"{points.Count}\n{ink.Strokes.Count}");    
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ink.Strokes.Clear();           
         }
     }
 }
